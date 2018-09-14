b872a2534311dbc145dfba0cbeef7f764a6482bc
shanmughamks <ks_shan@hotmail.com>
2018-09-14 16:21:01 -0400

    Add files via upload

---
+++ AngularProject/AngularProject/Scripts/MyAngularScript.js
@@ -0,0 +1,9 @@
+﻿var ngApp = angular.module("myApp", []);
+
+ngApp.controller("myController", function ($scope) {
+    $scope.message = "This is my first Angular Project !";
+
+    $scope.DisplayMessage = function () {
+        alert("You clicked me !");
+    }
+});
